f3b6050131716d610c94fd2ef63688cf63fad346
Nazar Leush <n.leush@gmail.com>
2014-11-26 17:54:04 +0200

    move oembed files to package

--- packages/itteco:iframely/package.js
+++ packages/itteco:iframely/package.js
@@ -1,17 +1,19 @@
 Package.describe({
   name: 'itteco:iframely',
   summary: ' /* Fill me in! */ ',
-  version: '1.0.0',
+  version: '0.0.1',
   git: ' /* Fill me in! */ '
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.2');
-  api.addFiles('itteco:iframely.js');
-});
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('itteco:iframely');
-  api.addFiles('itteco:iframely-tests.js');
+  api.versionsFrom('METEOR@0.9.2');
+
+  api.use('templating');
+
+  api.addFiles('lib/client/embed.html', 'client');
+  api.addFiles('lib/client/embed.js', 'client');
+
+  api.addFiles('lib/server/embed.js', 'server');
+
 });
